e78e8f8618300ddf1fb624203419ea28fe933cc0
NicolasArroyo <nicolasmach07@gmail.com>
2023-09-24 10:43:48 -0500

    new constructor for record

--- SeqFile/Record.cpp
+++ SeqFile/Record.cpp
@@ -9,6 +9,9 @@ std::string Record::toString() {
 
 Record::Record(int id, std::string name, std::string lastname) {
     this->id = id;
-    strcpy_s(this->name, "TEST");
-    strcpy_s(this->last_name, "TEST");
+    std::strncpy(this->name, name.c_str(), sizeof(name) - 1);
+    std::strncpy(this->last_name, lastname.c_str(), sizeof(last_name) - 1);
+
+    name[sizeof(name) - 1] = '\0';
+    last_name[sizeof(last_name) - 1] = '\0';;
 }
